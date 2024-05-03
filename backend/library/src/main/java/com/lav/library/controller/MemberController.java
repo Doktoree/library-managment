3c251af8288e56af170ed40d6eefca79828e5ab9
doktoree <tjuring901@gmail.com>
2024-05-03 14:00:00 +0200

    Implementirana sistemska operacija KreirajClana

--- backend/library/src/main/java/com/lav/library/controller/MemberController.java
+++ backend/library/src/main/java/com/lav/library/controller/MemberController.java
@@ -6,6 +6,7 @@ package com.lav.library.controller;
 
 import com.lav.library.dto.MemberDto;
 import com.lav.library.service.MemberService;
+import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -29,22 +30,58 @@ public class MemberController {
     @PostMapping
     public ResponseEntity<?> createMember(@RequestBody MemberDto memberDto) {
 
-        if(memberDto.getFirstName() == null || memberDto.getLastName() == null || memberDto.getBirthDate() == null 
-              || memberDto.getAdress() == null || memberDto.getPhoneNumber() == null){
-            
-            return ResponseEntity.badRequest().body("All fields are required!");
-        }
+        String validationMessage = validateMemberDto(memberDto);
         
-        if(!(memberDto.getFirstName() instanceof String) || memberDto.getFirstName().isEmpty()){
-            
-            return ResponseEntity.badRequest().body("First name should not be empty!");
-            
-        }
-        
+        if(validationMessage != null)
+            return ResponseEntity.badRequest().body(validationMessage);
+
         MemberDto createdMemberDto = memberService.createMember(memberDto);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(createdMemberDto);
 
     }
+
+    private String validateMemberDto(MemberDto memberDto) {
+        
+        if (memberDto.getFirstName() == null || memberDto.getLastName() == null || memberDto.getBirthDate() == null
+                || memberDto.getAdress() == null || memberDto.getPhoneNumber() == null) {
+
+            return "All fields are required!";
+        }
+
+        if (!(memberDto.getFirstName() instanceof String) || memberDto.getFirstName().isEmpty()) {
+
+            return "First name should not be empty!";
+
+        }
+
+        if (!(memberDto.getLastName() instanceof String) || memberDto.getLastName().isEmpty()) {
+
+            return "Last name should not be empty!";
+
+        }
+
+        if (!(memberDto.getBirthDate() instanceof LocalDate) || memberDto.getBirthDate().isAfter(LocalDate.now())) {
+
+            return "Birth date must be a valid date in the past.";
+
+        }
+
+        if (!(memberDto.getAdress() instanceof String) || memberDto.getLastName().isEmpty()) {
+
+            return "Adress should not be empty!";
+
+        }
+
+        if (!(memberDto.getPhoneNumber() instanceof String) || memberDto.getPhoneNumber().isEmpty()) {
+
+            return "Phone number should not be empty!";
+
+        }
+        
+        return null;
+    }
+    
+    
 
 }
